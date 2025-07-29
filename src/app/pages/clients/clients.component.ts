0298b6fb611c596c171e73140b024ab66e3aa494
MohammadAbsar <absar.sarwar@wordsensesoft.net>
2025-07-29 21:52:32 +0500

    changes

--- src/app/pages/clients/clients.component.ts
+++ src/app/pages/clients/clients.component.ts
@@ -6,59 +6,72 @@ import { Tab, Supplier } from '../../Interfaces/Model';
   selector: 'app-clients',
   imports: [CommonModule],
   templateUrl: './clients.component.html',
-  styleUrl: './clients.component.scss'
+  styleUrl: './clients.component.scss',
 })
 export class ClientsComponent {
-   activeTab = "clients"
+  activeTab = 'clients';
 
   tabs: Tab[] = [
-    { id: "clients", label: "Valued Clients" },
-    { id: "suppliers", label: "Suppliers" },
-    { id: "banks", label: "Banking Partners" },
-  ]
+    { id: 'clients', label: 'Valued Clients' },
+    { id: 'suppliers', label: 'Suppliers' },
+    { id: 'banks', label: 'Banking Partners' },
+  ];
 
   clients = [
-    "A J Match Factory, Sheikhupura",
-    "Mohsin Match Factory, Peshawar",
-    "Alam Match Factory, Peshawar",
-    "Khyber Match Factory",
-    "Popular Match Factory",
-    "Dawn Paper Mart, Karachi",
-    "Kashif Impex, Lahore",
-    "Bao Brothers",
-    "Al Aziz Packages",
-    "Khurshid Impex",
-    "Artotec (pvt) Ltd",
-    "Modern Paper Mart, Lahore",
-    "M. N. Paper Mart, Karachi",
-    "A H Paper, Lahore",
-    "Pakistan International Printing",
-    "Royal Paper Mart, Rawalpindi",
-    "Malik Paper Trader, Rawalpindi",
-    "Malik Paper Mart, Rawalpindi",
-  ]
+    'A J Match Factory, Sheikhupura',
+    'Mohsin Match Factory, Peshawar',
+    'Alam Match Factory, Peshawar',
+    'Khyber Match Factory',
+    'Popular Match Factory',
+    'Dawn Paper Mart, Karachi',
+    'Kashif Impex, Lahore',
+    'Bao Brothers',
+    'Al Aziz Packages',
+    'Khurshid Impex',
+    'Artotec (pvt) Ltd',
+    'Modern Paper Mart, Lahore',
+    'M. N. Paper Mart, Karachi',
+    'A H Paper, Lahore',
+    'Pakistan International Printing',
+    'Royal Paper Mart, Rawalpindi',
+    'Malik Paper Trader, Rawalpindi',
+    'Malik Paper Mart, Rawalpindi',
+  ];
 
   internationalSuppliers: Supplier[] = [
-    { name: "Hansol Chemical Co. Ltd", location: "Korea - Import" },
-    { name: "BASF South East Asia Pte Ltd", location: "Singapore - Import" },
-  ]
+    // { name: 'Hansol Chemical Co. Ltd', location: 'Korea - Import' },
+    // { name: 'BASF South East Asia Pte Ltd', location: 'Singapore - Import' },
+
+    { name: 'Thiele Kaolin Company', location: 'Korea - Import' },
+    { name: 'Hansol Chemical Co. Ltd', location: 'Korea - Import' },
+    { name: 'Nantex Industry Co. Ltd', location: 'Korea - Import' },
+    { name: 'Saj International Trading Pte Ltd', location: 'Korea - Import' },
+    { name: 'Valtec Engineering Ltd', location: 'Korea - Import' },
+    { name: 'Alliance International DWC-LLC', location: 'Korea - Import' },
+    { name: 'Saj International Trading Pte Ltd', location: 'Korea - Import' },
+    {
+      name: 'Global Leyi New Materials Technology (NINGBO) Co.Ltd',
+      location: 'Korea - Import',
+    },
+    { name: 'BASF Pakistan (Pvt) Limited', location: 'Korea - Import' },
+    { name: 'Buckman Laboratorie', location: 'Korea - Import' },
+  ];
 
   localSuppliers = [
-    "Ideal Chemicals (pvt) Ltd",
-    "Sitara Chemical (pvt) Ltd",
-    "Waseem Trader – Waste Supplier",
-    "Jutt Trader – Waste Supplier",
-    "Ali Baba Enterprise – Waste Supplier",
-  ]
+    'Archroma Pakistan (pvt) Ltd',
+    'Sitara Chemical (pvt) Ltd',
+    'Rafhan (pvt) Ltd',
+    'Nimir (pvt) Ltd',
+  ];
 
   banks = [
-    "Bank Al Falah, F/7 Markaz Branch, Islamabad",
-    "Bank Al Habib, Industrial Area Branch, Hattar",
-    "Silk Bank, Razia Sharif Plaza, Blue Area, Islamabad",
-    "Meezan Bank, F/7 Markaz Branch, Islamabad",
-    "HBL Main Branch, Blue Area, Islamabad",
-    "UBL F/7 Markaz Branch, Islamabad",
-    "BOK Blue Area Branch, Islamabad",
-    "BOP Blue Area Branch, Islamabad",
-  ]
+    'Bank Al Falah, F/7 Markaz Branch, Islamabad',
+    'Bank Al Habib, Industrial Area Branch, Hattar',
+    'Silk Bank, Razia Sharif Plaza, Blue Area, Islamabad',
+    'Meezan Bank, F/7 Markaz Branch, Islamabad',
+    'HBL Main Branch, Blue Area, Islamabad',
+    'UBL F/7 Markaz Branch, Islamabad',
+    'BOK Blue Area Branch, Islamabad',
+    'BOP Blue Area Branch, Islamabad',
+  ];
 }
